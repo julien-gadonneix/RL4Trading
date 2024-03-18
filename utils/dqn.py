e35d58b5e24a6b2e213a79bc52be279edac2e1f7
mathias-grau <mathias4grau@gmail.com>
2024-03-18 16:19:12 +0100

    create class transformer

--- utils/dqn.py
+++ utils/dqn.py
@@ -38,3 +38,35 @@ class DQN(nn.Module):
         x = torch.sigmoid(self.fc2(x))
         return x
 
+class DQN_with_Transformer(nn.Module):
+    def __init__(self, input_size_price_list, input_size_balance_value, input_size_num_of_shares, device):
+        super(DQN_with_Transformer, self).__init__()
+        self.device = device
+        self.output_lstm_shape = 64
+        self.hidden_layer_shape = 32
+        self.output_shape = 3
+        self.numlayers = 2
+        self.transformer = nn.Transformer(nhead=1, num_encoder_layers=1, num_decoder_layers=1, dim_feedforward=32, dropout=0.1)
+        self.fc1 = nn.Linear(input_size_price_list + input_size_balance_value + input_size_num_of_shares, self.hidden_layer_shape)
+        self.fc2 = nn.Linear(self.hidden_layer_shape, self.output_shape)
+
+    def forward(self, x_price_tensor, x_portfolio_value_tensor, x_num_of_shares_tensor):
+        """_summary_
+
+        Args:
+            x_price_tensor (torch.tensor): list of T last stock prices
+            x_portfolio_value_tensor (torch.tensor): value of the balance at time t
+            x_num_of_shares_tensor (torch.tensor): value of the number of share at time t
+
+        Returns:
+            torch.tensor: the action to be taken between -1 and 1 
+        """
+        x = x_price_tensor
+        x = self.transformer(x, x)
+        # add the portfolio value and the number of shares to the input
+        x_portfolio_value_tensor = x_portfolio_value_tensor.view(-1, 1)
+        x_num_of_shares_tensor = x_num_of_shares_tensor.view(-1, 1, )
+        x = torch.cat((x, x_portfolio_value_tensor, x_num_of_shares_tensor), 1)
+        x = F.relu(self.fc1(x))
+        x = torch.sigmoid(self.fc2(x))
+        return x
